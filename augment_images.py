a184b235d4429ef4235be14d996241d55f727093
Fabian <besserer.fabian@gmail.com>
2022-04-22 23:42:13 +0200

    finishing preparation to add more augmentation modules/methods by saving translated images as Img objects in an image collection as a local variable in translate_images() for further processing

--- augment_images.py
+++ augment_images.py
@@ -9,10 +9,12 @@ import csv
 NEW_IMAGE_SUFFIX = "_flipped.jpg"
 # NEW_ANNOTATION_SUFFIX = "_flipped.txt"
 # INITDIR = "F:\\RadarProjekt\\Training"
+
+
 INITDIR = "C:\\Users\\Fabian\\Documents\\MCSc\\Projekt\\Code\\DataAugmentation"
 
 
-class Images:
+class Executor:
     def __init__(self, origin_path, save_path):
         self.origin_path = origin_path
         self.save_path = save_path
@@ -31,10 +33,10 @@ class Images:
 
 
 class Img:
-    def __init__(self, name):
-        self.binaries = None
+    def __init__(self, name, binaries=None, annotations=None):
+        self.binaries = binaries
         self.name = name
-        self.annotations = []
+        self.annotations = [] if annotations is None else annotations
 
 
 class Augmentation:
@@ -45,7 +47,7 @@ class Augmentation:
         self.origin_image_size = 448
         self.final_image_size = 416
 
-    def save_image(self, name, file, quality):
+    def save_image(self, file, name, quality):
         pass
         # (1, 100) -> IMWRITE_JPEG_QUALITY 100 (highest)
         # cv2.imwrite(self.origin_path + image_name + NEW_IMAGE_SUFFIX, self.image_flipped, (1, 100))
@@ -71,122 +73,131 @@ class Augmentation:
                 bin_image_flipped.annotations.append(new_row)
 
     def translate_images(self, image_binaries):
-        # for nr, file in enumerate(self.files):
+        image_collection = []
         for image in image_binaries:
-            # if file.endswith(".jpg"):
-            # im = cv2.imread(self.origin_path + file, flags=-1)
-            # image_name = file[:-4]
-            # self.annotation_file_path = image_name + ".txt"
-
             translation1 = image.binaries[0:416, 0:416, :]  # row, column, depth
-            cv2.imwrite(self.save_path + image.name + "tr1.jpg", translation1, (1, 100))
-            self.redraw_annotations("tr1.txt", upper=True, left=True)
+            cv2.imwrite(self.save_path + image.name + "_tr1.jpg", translation1, (1, 100))
+            annotations = self.redraw_annotations(image, upper=True, left=True)
+            self.save_txt(annotations, image.name + "_tr1.txt")
+            image_collection.append(Img(image.name + "_tr1", translation1, annotations))
 
             translation2 = image.binaries[32:448, 0:416, :]
-            cv2.imwrite(self.save_path + image.name + "tr2.jpg", translation2, (1, 100))
-            self.redraw_annotations("tr2.txt", False, True)
+            cv2.imwrite(self.save_path + image.name + "_tr2.jpg", translation2, (1, 100))
+            annotations = self.redraw_annotations(image, False, True)
+            self.save_txt(annotations, image.name + "_tr2.txt")
+            image_collection.append(Img(image.name + "_tr2", translation2, annotations))
 
             translation3 = image.binaries[0:416, 32:448, :]
-            cv2.imwrite(self.save_path + image.name + "tr3.jpg", translation3, (1, 100))
-            self.redraw_annotations("tr3.txt", True, False)
+            cv2.imwrite(self.save_path + image.name + "_tr3.jpg", translation3, (1, 100))
+            annotations = self.redraw_annotations(image, True, False)
+            self.save_txt(annotations, image.name + "_tr3.txt")
+            image_collection.append(Img(image.name + "_tr3", translation3, annotations))
 
             translation4 = image.binaries[32:448, 32:448, :]
-            cv2.imwrite(self.save_path + image.name + "tr4.jpg", translation4, (1, 100))
-            self.redraw_annotations("tr4.txt", False, False)
+            cv2.imwrite(self.save_path + image.name + "_tr4.jpg", translation4, (1, 100))
+            annotations = self.redraw_annotations(image, False, False)
+            self.save_txt(annotations, image.name + "_tr4.txt")
+            image_collection.append(Img(image.name + "_tr4", translation4, annotations))
 
             translation5 = image.binaries[16:432, 16:432, :]
-            cv2.imwrite(self.save_path + image.name + "tr5.jpg", translation5, (1, 100))
-            self.redraw_annotations("tr5.txt", center=True)
+            cv2.imwrite(self.save_path + image.name + "_tr5.jpg", translation5, (1, 100))
+            annotations = self.redraw_annotations(image, center=True)
+            self.save_txt(annotations, image.name + "_tr5.txt")
+            image_collection.append(Img(image.name + "_tr5", translation5, annotations))
 
-    def redraw_annotations(self, suffix, upper=False, left=False, center=False):
-        with open(self.save_path + self.annotation_file_path[:-4] + suffix, mode='w',
-                  newline='') as new_annotation_file:
-            with open(self.origin_path + self.annotation_file_path, mode='r') as old_annotation_file:
-                reader = csv.reader(old_annotation_file, delimiter=" ")
-                writer = csv.writer(new_annotation_file, delimiter=" ")
-                for row in reader:
-                    # <object-class> <x_center> <y_center> <width> <height>
-                    x_center = float(row[1]) * self.origin_image_size
-                    y_center = float(row[2]) * self.origin_image_size
-                    width = float(row[3]) * self.origin_image_size
-                    height = float(row[4]) * self.origin_image_size
+    def save_txt(self, annotations, filename):
+        with open(self.save_path + filename, mode='w', newline='') as new_annotation_file:
+            writer = csv.writer(new_annotation_file, delimiter=" ")
+            for row in annotations:
+                writer.writerow(row)
 
-                    if center:
-                        x_center_new = (x_center - 16) / self.final_image_size
-                        if x_center + 0.5 * width > self.final_image_size + 16:
-                            width -= x_center + 0.5 * width - (self.final_image_size + 16)
-                            if width < 0:
-                                continue
-                            x_center_new = (self.final_image_size - (0.5 * width)) / self.final_image_size
-                        elif x_center - 0.5 * width < 16:
-                            width -= 16 - (x_center - 0.5 * width)
-                            if width < 0:
-                                continue
-                            x_center_new = (0.5 * width) / self.final_image_size
+    def redraw_annotations(self, image, upper=False, left=False, center=False):
+        annotations = []
+        for row in image.annotations:
+            # <object-class> <x_center> <y_center> <width> <height>
+            x_center = float(row[1]) * self.origin_image_size
+            y_center = float(row[2]) * self.origin_image_size
+            width = float(row[3]) * self.origin_image_size
+            height = float(row[4]) * self.origin_image_size
 
-                        y_center_new = (y_center - 16) / self.final_image_size
-                        if y_center + 0.5 * height > self.final_image_size + 16:
-                            height -= y_center + 0.5 * height - (self.final_image_size + 16)
-                            if height < 0:
-                                continue
-                            y_center_new = (self.final_image_size - (0.5 * height)) / self.final_image_size
-                        elif y_center - 0.5 * height < 16:
-                            height -= 16 - (y_center - 0.5 * height)
-                            if height < 0:
-                                continue
-                            y_center_new = (0.5 * height) / self.final_image_size
+            if center:  # (translation 5)
+                x_center_new = (x_center - 16) / self.final_image_size
+                if x_center + 0.5 * width > self.final_image_size + 16:
+                    width -= x_center + 0.5 * width - (self.final_image_size + 16)
+                    if width < 0:
+                        continue
+                    x_center_new = (self.final_image_size - (0.5 * width)) / self.final_image_size
+                elif x_center - 0.5 * width < 16:
+                    width -= 16 - (x_center - 0.5 * width)
+                    if width < 0:
+                        continue
+                    x_center_new = (0.5 * width) / self.final_image_size
 
-                        print(
-                            f"{self.annotation_file_path}: "
-                            f"<{round(x_center_new * self.final_image_size - (width / 2), 1)}> "
-                            f"<{round(x_center_new * self.final_image_size + (width / 2), 1)}> "
-                            f"<{round(y_center_new * self.final_image_size - (height / 2), 1)}> "
-                            f"<{round(y_center_new * self.final_image_size + (height / 2), 1)}>")
-                    else:
-                        if left:
-                            x_center_new = x_center / self.final_image_size
-                            if x_center + 0.5 * width > self.final_image_size:  # 410 + 10 = 420
-                                width -= x_center + 0.5 * width - self.final_image_size  # 20 - (410 + 10 - 416) = 16
-                                if width < 0:
-                                    continue
-                                x_center_new = (self.final_image_size - (0.5 * width)) / self.final_image_size
-                        else:
-                            x_center_new = (x_center - 32) / self.final_image_size
-                            if x_center - 0.5 * width < 32:
-                                width -= 32 - (x_center - 0.5 * width)  # 20 - ( 32 - (40 - 10))
-                                if width < 0:
-                                    continue
-                                x_center_new = (0.5 * width) / self.final_image_size
+                y_center_new = (y_center - 16) / self.final_image_size
+                if y_center + 0.5 * height > self.final_image_size + 16:
+                    height -= y_center + 0.5 * height - (self.final_image_size + 16)
+                    if height < 0:
+                        continue
+                    y_center_new = (self.final_image_size - (0.5 * height)) / self.final_image_size
+                elif y_center - 0.5 * height < 16:
+                    height -= 16 - (y_center - 0.5 * height)
+                    if height < 0:
+                        continue
+                    y_center_new = (0.5 * height) / self.final_image_size
 
-                        if upper:
-                            y_center_new = y_center / self.final_image_size
-                            if y_center + 0.5 * height > self.final_image_size:
-                                height -= y_center + 0.5 * height - self.final_image_size
-                                if height < 0:
-                                    continue
-                                y_center_new = (self.final_image_size - (0.5 * height)) / self.final_image_size
-                        else:
-                            y_center_new = (y_center - 32) / self.final_image_size
-                            if y_center - 0.5 * height < 32:
-                                height -= 32 - (y_center - 0.5 * height)
-                                if height < 0:
-                                    continue
-                                y_center_new = (0.5 * height) / self.final_image_size
+                # print(
+                #     f"{image.name}: "
+                #     f"<{round(x_center_new * self.final_image_size - (width / 2), 1)}> "
+                #     f"<{round(x_center_new * self.final_image_size + (width / 2), 1)}> "
+                #     f"<{round(y_center_new * self.final_image_size - (height / 2), 1)}> "
+                #     f"<{round(y_center_new * self.final_image_size + (height / 2), 1)}>")
+            else:
+                if left:
+                    x_center_new = x_center / self.final_image_size
+                    if x_center + 0.5 * width > self.final_image_size:  # 410 + 10 = 420
+                        width -= x_center + 0.5 * width - self.final_image_size  # 20 - (410 + 10 - 416) = 16
+                        if width < 0:
+                            continue
+                        x_center_new = (self.final_image_size - (0.5 * width)) / self.final_image_size
+                else:
+                    x_center_new = (x_center - 32) / self.final_image_size
+                    if x_center - 0.5 * width < 32:
+                        width -= 32 - (x_center - 0.5 * width)  # 20 - ( 32 - (40 - 10))
+                        if width < 0:
+                            continue
+                        x_center_new = (0.5 * width) / self.final_image_size
 
-                    width = min(width / self.final_image_size, 1)
-                    height = min(height / self.final_image_size, 1)
+                if upper:
+                    y_center_new = y_center / self.final_image_size
+                    if y_center + 0.5 * height > self.final_image_size:
+                        height -= y_center + 0.5 * height - self.final_image_size
+                        if height < 0:
+                            continue
+                        y_center_new = (self.final_image_size - (0.5 * height)) / self.final_image_size
+                else:
+                    y_center_new = (y_center - 32) / self.final_image_size
+                    if y_center - 0.5 * height < 32:
+                        height -= 32 - (y_center - 0.5 * height)
+                        if height < 0:
+                            continue
+                        y_center_new = (0.5 * height) / self.final_image_size
 
-                    new_row = [row[0], "{:.6f}".format(x_center_new), "{:.6f}".format(y_center_new),
-                               "{:.6f}".format(width), "{:.6f}".format(height)]
-                    writer.writerow(new_row)
+            width = min(width / self.final_image_size, 1)
+            height = min(height / self.final_image_size, 1)
+
+            new_row = [row[0], "{:.6f}".format(x_center_new), "{:.6f}".format(y_center_new),
+                       "{:.6f}".format(width), "{:.6f}".format(height)]
+            annotations.append(new_row)
+        return annotations
 
 
 def delete_files(path):
-    confirm = askyesno("Warning", f"Do you want to delete all files in path {path} with \"translated\" in their name?")
+    delstr = "_flipped"
+    confirm = askyesno("Warning", f"Do you want to delete all files in path {path} with \"{delstr}\" in their name?")
     if confirm:
         files = os.listdir(path)
         for file in files:
-            if "translated" in file:
+            if delstr in file:
                 os.remove(path + "\\" + file)
 
 
@@ -197,7 +208,5 @@ if __name__ == "__main__":
                                            title="Ordner auswählen, an dem augmentierte Bilder gespeichert werden sollen")
     save_path += "\\"
     # delete_files(original_path)
-    # flip_images_horizontally(original_path, os.listdir(original_path))
-    # create_annotation_file()
-    aug = Images(original_path, save_path)
+    aug = Executor(original_path, save_path)
     aug.start_augmentation()
