1ea056d86fa64809f57b883aec0a1a75840c00b0
Fabian <besserer.fabian@gmail.com>
2022-06-08 10:52:38 +0200

    added no_blur and annotation methods fixed iou method -> overlap method

--- SyntheticImages/main.py
+++ SyntheticImages/main.py
@@ -1,14 +1,15 @@
+import csv
 from collections import namedtuple
 from queue import Queue
 import tkinter as tk
 from tkinter import filedialog
-from typing import List
+from typing import List, Optional, Tuple
 import numpy as np
 from PIL import Image
 import cv2
 
 from fg_pipeline import FGExecutor, Foreground
-from bg_pipeline import BGExecutor, Background
+from bg_pipeline import BGExecutor, Background, bounding_box
 
 INITDIR_FG = "F:\\RadarProjekt\\Synthetische Bilder\\freigestellte Blitzer"
 # INITDIR_BG = "F:\\RadarProjekt\\Training\\Training"
@@ -18,8 +19,13 @@ PIXELS = 416
 ForegroundSize: namedtuple = namedtuple("ForegroundSize", ["height", "width"])
 
 
-class CombinedImage:
-    pass
+class CombinedImage:  # als Superklasse für background festlegen wg. iou funktion?
+    def __init__(self, name: str, binaries: np.ndarray, annotations: Optional[List[Tuple[float, ...]]] = None,
+                 bounding_box_corners: Optional[List[namedtuple]] = None):
+        self.name = name
+        self.binaries = binaries
+        self.annotations = [] if annotations is None else annotations  # original txt bbox content to generate new annotation file
+        self.bounding_box_corners = [] if bounding_box_corners is None else bounding_box_corners
 
 
 class Executor:
@@ -34,6 +40,7 @@ class Executor:
         while not self.foregrounds.empty():
             ImageCombinator(self.foregrounds.get(), self.backgrounds.get(), self.save_path,
                             self.distractor_objects).fuse_images()
+            print(self.foregrounds.unfinished_tasks)
 
 
 class ImageCombinator:
@@ -45,13 +52,12 @@ class ImageCombinator:
         self.save_path = save_path
 
     def fuse_images(self) -> None:
-        # 3 annotation files müssen generiert werden
-        # 3 bilder müssen erzeugt werden
         # 1 methode für truncation, die randpixelwerte zum einfügen zurück gibt, dann muss geprüft werden ob der blitzer dort etwas verdecjt
         # 1 methode
         # position zufällig auswählen, sodass foreground objekt rein passt
         if np.random.randint(0, 10) < 2:
-            self.fuse_truncated()
+            # self.fuse_truncated()
+            self.fuse()
         else:
             combined_image = self.fuse()
         # 50% der Fälle random element aus distractor liste rausholen -> distractor müssen bei allen 3 bildern eingefügt werden
@@ -66,16 +72,57 @@ class ImageCombinator:
             fg_position_x_in_bg = np.random.randint(0, PIXELS - fgsize.width)
             fg_position_y_in_bg = np.random.randint(0, PIXELS - fgsize.height)
             # max 75% overlap
-            if self.intersection_over_union(self.background, fg_position_x_in_bg, fg_position_y_in_bg, fgsize) < 0.75:
+            if self.overlap(self.background, fg_position_x_in_bg, fg_position_y_in_bg, fgsize) < 0.75:
                 # fusing auf 3 arten
-                noblur: CombinedImage = self.no_blur(fg_position_x_in_bg, fg_position_y_in_bg, fgsize)
+                noblur: CombinedImage = self.no_blur(fg_position_x_in_bg, fg_position_y_in_bg)
+                self.add_annotations(noblur, fg_position_x_in_bg, fg_position_y_in_bg, fgsize)
+                self.add_bboxes(noblur, fg_position_x_in_bg, fg_position_y_in_bg, fgsize)
+                noblur.binaries = cv2.cvtColor(noblur.binaries, cv2.COLOR_RGB2BGR)
+                cv2.imwrite(self.save_path + noblur.name + ".jpg", noblur.binaries)
+                self.save_annotation_file(noblur.annotations, noblur.name)
+
                 # gauss: CombinedImage = self.gaussian(fg_position_x_in_bg, fg_position_y_in_bg, fgsize)
                 # poiss: CombinedImage = self.poisson(fg_position_x_in_bg, fg_position_y_in_bg, fgsize)
                 break
 
-    def intersection_over_union(self, image: Background, position_x: int, position_y: int,
-                                size: ForegroundSize) -> float:
-        # calculate iou for all potential bboxes
+    def no_blur(self, fg_position_x_in_bg: int, fg_position_y_in_bg: int) -> CombinedImage:
+        mask = self.foreground.binaries[:, :, 3]  # alpha Kanal
+        mask = Image.fromarray(mask)
+        # cv2 default is BGR --> conversion to RGB(A) for PIL
+        bg = cv2.cvtColor(self.background.binaries, cv2.COLOR_BGR2RGB)
+        fg = cv2.cvtColor(self.foreground.binaries, cv2.COLOR_BGRA2RGBA)
+        bg = Image.fromarray(bg)
+        fg = Image.fromarray(fg)
+        bg.paste(fg, box=(fg_position_x_in_bg, fg_position_y_in_bg), mask=mask)
+        # bg.save("F:\\RadarProjekt\\Synthetische Bilder\\freigestellte Blitzer\\BlitzertestPILnoblur.jpg")
+        bg = np.array(bg)
+        return CombinedImage(self.background.name + self.foreground.name + "_noblur", bg)
+
+    def add_annotations(self, comb_image: CombinedImage, fg_position_x_in_bg: int, fg_position_y_in_bg: int,
+                        fgsize: ForegroundSize) -> None:
+        # bg annotations
+        comb_image.annotations = [annot for annot in self.background.annotations]
+        # fg annotations yolo format:
+        # class, x (center), y (center), width, height - all relative to PIXELS
+        # ex: 3, 0.543270, 0.633413, 0.125000, 0.733173
+        comb_image.annotations.append(
+            (self.foreground.annot_class,
+             str(round((fgsize.width / 2 + fg_position_x_in_bg) / PIXELS, 6)),
+             str(round((fgsize.height / 2 + fg_position_y_in_bg) / PIXELS, 6)),
+             str(round(fgsize.width / PIXELS, 6)),
+             str(round(fgsize.height / PIXELS, 6))))
+
+    def add_bboxes(self, comb_image: CombinedImage, fg_position_x_in_bg: int, fg_position_y_in_bg: int,
+                   fgsize: ForegroundSize) -> None:
+        # bg bboxes
+        comb_image.bounding_box_corners = [corners for corners in self.background.bounding_box_corners]
+        # fg bboxes
+        comb_image.bounding_box_corners.append(
+            bounding_box(fg_position_x_in_bg, fg_position_x_in_bg + fgsize.width, fg_position_y_in_bg,
+                         fg_position_y_in_bg + fgsize.height))
+
+    def overlap(self, image: Background, position_x: int, position_y: int, size: ForegroundSize) -> float:
+        # calculate overlap for all potential bboxes
         max_overlap: float = 0
         erase_x1 = position_x
         erase_x2 = position_x + size.width
@@ -88,26 +135,21 @@ class ImageCombinator:
             y2 = min(erase_y2, bbox.y2)
 
             intersection_area = max(0, x2 - x1 + 1) * max(0, y2 - y1 + 1)
-            erase_area = (erase_x2 - erase_x1 + 1) * (erase_y2 - erase_y1 + 1)
+            # erase_area = (erase_x2 - erase_x1 + 1) * (erase_y2 - erase_y1 + 1)
             bbox_area = (bbox.x2 - bbox.x1 + 1) * (bbox.y2 - bbox.y1 + 1)
-            iou = intersection_area / (erase_area + bbox_area - intersection_area)
-            max_overlap = iou if iou > max_overlap else max_overlap
+            # iou = intersection_area / (erase_area + bbox_area - intersection_area)
+            # max_overlap = iou if iou > max_overlap else max_overlap
+
+            overlap = intersection_area / bbox_area
+            max_overlap = overlap if overlap > max_overlap else max_overlap
 
         return max_overlap
 
-    def no_blur(self, fg_position_x_in_bg: int, fg_position_y_in_bg: int, fgsize: ForegroundSize) -> CombinedImage:
-        mask = self.foreground.binaries[:, :, 3]  # alpha Kanal
-        mask = Image.fromarray(mask)
-        # cv2 default is BGR --> conversion to RGB(A) for PIL
-        bg = cv2.cvtColor(self.background.binaries, cv2.COLOR_BGR2RGB)
-        bg = Image.fromarray(bg)
-        fg = cv2.cvtColor(self.foreground.binaries, cv2.COLOR_BGRA2RGBA)
-        fg = Image.fromarray(fg)
-        bg.paste(fg, box=(fg_position_x_in_bg, fg_position_y_in_bg), mask=mask)
-        # bg.save("F:\\RadarProjekt\\Synthetische Bilder\\freigestellte Blitzer\\BlitzertestPILnoblur.jpg")
-        CombinedImage erstellen
-        # background image annotations nehmen
-        # fg_pos und fgsize für annotations
+    def save_annotation_file(self, annotations, filename):
+        with open(self.save_path + filename + ".txt", mode='w', newline='') as new_annotation_file:
+            writer = csv.writer(new_annotation_file, delimiter=" ")
+            for row in annotations:
+                writer.writerow(row)
 
 
 if __name__ == "__main__":
@@ -127,3 +169,7 @@ if __name__ == "__main__":
         foreground_images, distractor_objects = FGExecutor(source_path_fg).execute()
         background_images: Queue = BGExecutor(source_path_bg).execute()
         Executor(foreground_images, background_images, save_path, distractor_objects).execute()
+
+    # bg_test = Background("test", np.zeros((10, 10)), bounding_box_corners=[bounding_box(200, 210, 200, 210)])
+    # size = ForegroundSize(20, 20)
+    # ImageCombinator(Queue(), Queue(), "test", None).overlap(bg_test, 200, 200, size)
