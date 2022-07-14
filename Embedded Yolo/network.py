1ff6571750ebcfeef73dca138d46a822a84421df
Fabian Besserer <besserer.fabian@gmail.com>
2022-07-14 10:35:23 +0200

    SqueezeExcitation Block

--- Embedded Yolo/network.py
+++ Embedded Yolo/network.py
@@ -22,31 +22,41 @@ def channel_shuffle(x: Tensor, groups: int) -> Tensor:
 
 
 class SqueezeExcitation(nn.Module):
-    def __init__(self, channels):
+    def __init__(self, channels, height_width):
         super(SqueezeExcitation, self).__init__()
         self.channels = channels
         self.ratio = 8
+        self.n: int = height_width
 
-    def forward(self, x: Tensor):  # x = BN Block
-        x = nn.AvgPool2d(self.channels)(x)
+        self.se = nn.Sequential(
+            nn.AvgPool2d(self.n),
+            nn.Linear(self.channels, self.channels // self.ratio, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(self.channels // self.ratio, self.channels, bias=False),
+            nn.Hardswish(inplace=True)
+        )
+
+    def forward(self, x: Tensor) -> Tensor:  # x = BN Block
+        assert x.shape[2] == x.shape[3]
+        return x * self.se(x)
 
 
 class AttentiveShuffleNetUnit(nn.Module):
-    def __init__(self, inp, oup, stride):
+    def __init__(self, in_channel, out_channel, height_width, stride):
         super(AttentiveShuffleNetUnit, self).__init__()
 
         if not (1 <= stride <= 3):
             raise ValueError('illegal stride value')
         self.stride = stride  # basic unit (stride = 1) or unit for spatial down sampling (stride=2)
 
-        branch_features = oup // 2
-        assert (self.stride != 1) or (inp == branch_features << 1)
+        branch_features = out_channel // 2
+        assert (self.stride != 1) or (in_channel == branch_features << 1)
 
         if self.stride > 1:
             self.branch1 = nn.Sequential(
-                self.depthwise_conv(inp, inp, kernel_size=3, stride=self.stride, padding=1),
-                nn.BatchNorm2d(inp),
-                nn.Conv2d(inp, branch_features, kernel_size=1, stride=1, padding=0, bias=False),
+                self.depthwise_conv(in_channel, in_channel, kernel_size=3, stride=self.stride, padding=1),
+                nn.BatchNorm2d(in_channel),
+                nn.Conv2d(in_channel, branch_features, kernel_size=1, stride=1, padding=0, bias=False),
                 nn.BatchNorm2d(branch_features),
                 nn.ReLU(inplace=True),
             )
@@ -54,7 +64,7 @@ class AttentiveShuffleNetUnit(nn.Module):
             self.branch1 = nn.Sequential()  # why?
 
         self.branch2 = nn.Sequential(
-            nn.Conv2d(inp if (self.stride > 1) else branch_features,
+            nn.Conv2d(in_channel if (self.stride > 1) else branch_features,
                       branch_features, kernel_size=1, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(branch_features),
             nn.ReLU(inplace=True),
@@ -64,8 +74,8 @@ class AttentiveShuffleNetUnit(nn.Module):
             nn.BatchNorm2d(branch_features),
             nn.ReLU(inplace=True),
         )
-
-        self.se = SqueezeExcitation(branch_features)  # 3 Blöcke für die unterschiedlichen Channels
+        if self.stride == 1:
+            self.se = SqueezeExcitation(branch_features, height_width)
 
     @staticmethod
     def depthwise_conv(i, o, kernel_size, stride=1, padding=0, bias=False):
@@ -74,10 +84,10 @@ class AttentiveShuffleNetUnit(nn.Module):
     def forward(self, x: Tensor) -> Tensor:
         if self.stride == 1:
             x1, x2 = x.chunk(2, dim=1)
-            # branch2 = self.branch2(x2)
-            # branch2 = self.se(branch2)
-            # out: Tensor = torch.cat((x1, branch2), dim=1)
-            out: Tensor = torch.cat((x1, self.branch2(x2)), dim=1)
+            branch2 = self.branch2(x2)
+            branch2 = self.se(branch2)
+            out: Tensor = torch.cat((x1, branch2), dim=1)
+            # out: Tensor = torch.cat((x1, self.branch2(x2)), dim=1)
         else:
             out: Tensor = torch.cat((self.branch1(x), self.branch2(x)), dim=1)
 
@@ -92,16 +102,18 @@ class EmbeddedYolo(nn.Module):
         asu = AttentiveShuffleNetUnit
         stages_repeats = [3, 7, 3]  # oder 4,8,4 wie im ShuffleNetv2?
         self._stage_out_channels = [116, 232, 464]
+        self.height_width = [52, 26, 13]
 
         self.conv1 = nn.Conv2d(3, 24, kernel_size=3, stride=2)  # bias = False? laut shufflenetv2 repo
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2)  # laut EY paper keine BN und relu Schicht hier
 
         input_channels = 24
         stage_names = [f'stage{i}' for i in [2, 3, 4]]
-        for name, repeats, output_channels in zip(stage_names, stages_repeats, self._stage_out_channels):
-            seq = [asu(input_channels, output_channels, stride=2)]
+        for name, repeats, output_channels, height_width in zip(stage_names, stages_repeats, self._stage_out_channels,
+                                                                self.height_width):
+            seq = [asu(input_channels, output_channels, height_width, stride=2)]
             for i in range(repeats - 1):
-                seq.append(asu(output_channels, output_channels, stride=1))
+                seq.append(asu(output_channels, output_channels, height_width, stride=1))
             setattr(self, name, nn.Sequential(*seq))  # Seq sorgt dafür dass beim späteren durchreichen die outputs an
             # die nächste repeat schicht weitergegeben wird
             input_channels = output_channels
